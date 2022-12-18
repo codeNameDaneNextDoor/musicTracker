fef9212f6e9e4b1dc53c245d088775c9ab680dc3
Dane Folk <112600116+codeNameDaneNextDoor@users.noreply.github.com>
2022-12-17 18:34:21 -0800

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Folk_CourseProject_Part1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleText = new System.Windows.Forms.TextBox();
+            this.artistText = new System.Windows.Forms.TextBox();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.genreText = new System.Windows.Forms.TextBox();
+            this.genreLabel = new System.Windows.Forms.Label();
+            this.yearText = new System.Windows.Forms.TextBox();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.songList = new System.Windows.Forms.ListBox();
+            this.allSongsButton = new System.Windows.Forms.Button();
+            this.songListLabel = new System.Windows.Forms.Label();
+            this.detailsLabel = new System.Windows.Forms.Label();
+            this.findButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.webViewDisplay = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.playButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(24, 56);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(75, 31);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title:";
+            // 
+            // titleText
+            // 
+            this.titleText.Location = new System.Drawing.Point(107, 53);
+            this.titleText.Margin = new System.Windows.Forms.Padding(4);
+            this.titleText.Name = "titleText";
+            this.titleText.Size = new System.Drawing.Size(316, 34);
+            this.titleText.TabIndex = 0;
+            // 
+            // artistText
+            // 
+            this.artistText.Location = new System.Drawing.Point(107, 134);
+            this.artistText.Margin = new System.Windows.Forms.Padding(4);
+            this.artistText.Name = "artistText";
+            this.artistText.Size = new System.Drawing.Size(316, 34);
+            this.artistText.TabIndex = 1;
+            this.artistText.TextChanged += new System.EventHandler(this.artistText_TextChanged);
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Location = new System.Drawing.Point(24, 134);
+            this.artistLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(87, 31);
+            this.artistLabel.TabIndex = 2;
+            this.artistLabel.Text = "Artist:";
+            // 
+            // genreText
+            // 
+            this.genreText.Location = new System.Drawing.Point(107, 221);
+            this.genreText.Margin = new System.Windows.Forms.Padding(4);
+            this.genreText.Name = "genreText";
+            this.genreText.Size = new System.Drawing.Size(316, 34);
+            this.genreText.TabIndex = 2;
+            // 
+            // genreLabel
+            // 
+            this.genreLabel.AutoSize = true;
+            this.genreLabel.Location = new System.Drawing.Point(24, 221);
+            this.genreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.genreLabel.Name = "genreLabel";
+            this.genreLabel.Size = new System.Drawing.Size(89, 31);
+            this.genreLabel.TabIndex = 4;
+            this.genreLabel.Text = "Genre:";
+            // 
+            // yearText
+            // 
+            this.yearText.Location = new System.Drawing.Point(107, 315);
+            this.yearText.Margin = new System.Windows.Forms.Padding(4);
+            this.yearText.Name = "yearText";
+            this.yearText.Size = new System.Drawing.Size(316, 34);
+            this.yearText.TabIndex = 3;
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(24, 315);
+            this.yearLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(73, 31);
+            this.yearLabel.TabIndex = 6;
+            this.yearLabel.Text = "Year:";
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(107, 404);
+            this.urlText.Margin = new System.Windows.Forms.Padding(4);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(316, 34);
+            this.urlText.TabIndex = 4;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(24, 404);
+            this.urlLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(70, 31);
+            this.urlLabel.TabIndex = 8;
+            this.urlLabel.Text = "URL:";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(136, 472);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(244, 66);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add Song";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(459, 392);
+            this.outputText.Margin = new System.Windows.Forms.Padding(4);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(365, 403);
+            this.outputText.TabIndex = 11;
+            this.outputText.TabStop = false;
+            // 
+            // songList
+            // 
+            this.songList.FormattingEnabled = true;
+            this.songList.HorizontalScrollbar = true;
+            this.songList.ItemHeight = 31;
+            this.songList.Location = new System.Drawing.Point(459, 56);
+            this.songList.Margin = new System.Windows.Forms.Padding(4);
+            this.songList.Name = "songList";
+            this.songList.ScrollAlwaysVisible = true;
+            this.songList.Size = new System.Drawing.Size(365, 283);
+            this.songList.TabIndex = 12;
+            this.songList.TabStop = false;
+            this.songList.SelectedIndexChanged += new System.EventHandler(this.songList_SelectedIndexChanged);
+            // 
+            // allSongsButton
+            // 
+            this.allSongsButton.Location = new System.Drawing.Point(136, 557);
+            this.allSongsButton.Margin = new System.Windows.Forms.Padding(4);
+            this.allSongsButton.Name = "allSongsButton";
+            this.allSongsButton.Size = new System.Drawing.Size(244, 66);
+            this.allSongsButton.TabIndex = 6;
+            this.allSongsButton.Text = "Show All Songs";
+            this.allSongsButton.UseVisualStyleBackColor = true;
+            this.allSongsButton.Click += new System.EventHandler(this.allSongsButton_Click);
+            // 
+            // songListLabel
+            // 
+            this.songListLabel.AutoSize = true;
+            this.songListLabel.Location = new System.Drawing.Point(571, 21);
+            this.songListLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.songListLabel.Name = "songListLabel";
+            this.songListLabel.Size = new System.Drawing.Size(118, 31);
+            this.songListLabel.TabIndex = 14;
+            this.songListLabel.Text = "Song List";
+            // 
+            // detailsLabel
+            // 
+            this.detailsLabel.AutoSize = true;
+            this.detailsLabel.Location = new System.Drawing.Point(586, 357);
+            this.detailsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.detailsLabel.Name = "detailsLabel";
+            this.detailsLabel.Size = new System.Drawing.Size(93, 31);
+            this.detailsLabel.TabIndex = 15;
+            this.detailsLabel.Text = "Details";
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(136, 644);
+            this.findButton.Margin = new System.Windows.Forms.Padding(4);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(244, 66);
+            this.findButton.TabIndex = 16;
+            this.findButton.Text = "Find Song";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(136, 729);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(244, 66);
+            this.clearButton.TabIndex = 17;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click_1);
+            // 
+            // webViewDisplay
+            // 
+            this.webViewDisplay.AllowExternalDrop = true;
+            this.webViewDisplay.CreationProperties = null;
+            this.webViewDisplay.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webViewDisplay.Location = new System.Drawing.Point(857, 53);
+            this.webViewDisplay.Name = "webViewDisplay";
+            this.webViewDisplay.Size = new System.Drawing.Size(561, 657);
+            this.webViewDisplay.Source = new System.Uri("https://www.microsoft.com", System.UriKind.Absolute);
+            this.webViewDisplay.TabIndex = 18;
+            this.webViewDisplay.ZoomFactor = 1D;
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(1023, 729);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(244, 66);
+            this.playButton.TabIndex = 19;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1444, 834);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.webViewDisplay);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.detailsLabel);
+            this.Controls.Add(this.songListLabel);
+            this.Controls.Add(this.allSongsButton);
+            this.Controls.Add(this.songList);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.yearText);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.genreText);
+            this.Controls.Add(this.genreLabel);
+            this.Controls.Add(this.artistText);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.titleText);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "MainForm";
+            this.Text = "Video Manager";
+            this.TransparencyKey = System.Drawing.Color.DimGray;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webViewDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox titleText;
+        private System.Windows.Forms.TextBox artistText;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.TextBox genreText;
+        private System.Windows.Forms.Label genreLabel;
+        private System.Windows.Forms.TextBox yearText;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.ListBox songList;
+        private System.Windows.Forms.Button allSongsButton;
+        private System.Windows.Forms.Label songListLabel;
+        private System.Windows.Forms.Label detailsLabel;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Button clearButton;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webViewDisplay;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace Folk_CourseProject_Part1
+{
+    public partial class MainForm : Form
+    {
+        // class level references
+        string[] titleArray = new string[5];
+        string[] artistArray = new string[5];
+        string[] genreArray = new string[5];
+        int[] yearArray = new int[5];
+        string[] urlArray = new string[5];
+        int songCount = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public bool ValidInput() 
+        {
+            // Return True if all fields are non-empty
+            bool isValid = true;
+
+            if (string.IsNullOrEmpty(titleText.Text))
+            {
+                //Title is Empty
+                MessageBox.Show("What Is The Name Of The Song?");
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(artistText.Text))
+            {
+                //Artist is Empty
+                MessageBox.Show("Who Wrote The Song?");
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(genreText.Text))
+            {
+                //Genre is Empty
+                MessageBox.Show("The Song Must Have A Genre!");
+                isValid=false;
+            }
+            else if (string.IsNullOrEmpty(yearText.Text))
+            {
+                //Year is Empty
+                MessageBox.Show("When Was The Song Released?");
+                isValid = false;
+            }
+            else if (string.IsNullOrEmpty(urlText.Text))
+            {
+                //URL is Empty
+                MessageBox.Show("Please Provide A Link To The Song!");
+                isValid = false;
+            }
+            return isValid;
+        }
+
+        private void artistText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(outputText.Text);
+            string nl = "\r\n";
+       
+            if (ValidInput())
+            {
+                // Add Title to list box and song list
+                songList.Items.Add(titleText.Text);
+                titleArray[songCount] = titleText.Text;
+                artistArray[songCount] = artistText.Text;
+                genreArray[songCount] = genreText.Text;
+                yearArray[songCount] = int.Parse(yearText.Text);
+                urlArray[songCount] = urlText.Text;
+
+                // increment song counter
+                songCount++;
+
+                //Build the output text
+                sb.Append(titleText.Text);
+                sb.Append(nl);
+                sb.Append(artistText.Text);
+                sb.Append(nl);
+                sb.Append(genreText.Text);
+                sb.Append(nl);
+                sb.Append(yearText.Text);
+                sb.Append(nl);
+                sb.Append(urlText.Text);
+                sb.Append(nl);
+                sb.Append(nl);          // blank line
+
+                outputText.Text = sb.ToString();
+
+                MessageBox.Show("Song Successfully Added!");
+            }
+        }
+
+        private bool SongInList(string songTitle)
+        {
+            bool found = false;
+
+            foreach (var item in songList.Items)
+            {
+                string currentSong = item as string;
+                // lowercase comparison not case sensitive
+                if(songTitle.ToLower() == currentSong.ToLower())
+                {
+                    found = true;
+                }
+            }
+
+            return found;
+            
+        }
+
+        private int GetSongIndex (string songTitle)
+        {
+            int songIndex = songList.Items.IndexOf(songTitle);
+            return songIndex;
+        }
+        private void songList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int songIndex = songList.SelectedIndex;
+
+            //if song is selected (index > -1), show details
+            if (songIndex >-1 )
+            {
+                StringBuilder sb = new StringBuilder(string.Empty);
+                string nl = "\r\n";
+
+                sb.Append(titleArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(artistArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(genreArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(yearArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(urlArray[songIndex]);
+                sb.Append(nl);
+
+                outputText.Text = sb.ToString();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void allSongsButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(string.Empty);
+            string nl = "\r\n";
+
+            //Build the output text
+            foreach (var item in songList.Items)
+            {
+                sb.Append(item.ToString());
+                sb.Append(nl);
+
+            }
+
+            // Put output text into output textbox
+            outputText.Text = sb.ToString();
+
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            if(SongInList(titleText.Text))
+            {
+                StringBuilder sb = new StringBuilder(string.Empty);
+                string nl = "\r\n";
+
+                int songIndex = GetSongIndex(titleText.Text);
+
+                //Build output text
+                sb.Append(titleArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(artistArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(genreArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(yearArray[songIndex]);
+                sb.Append(nl);
+                sb.Append(urlArray[songIndex]);
+                sb.Append(nl);
+
+                outputText.Text = sb.ToString();
+
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            int songIndex = songList.SelectedIndex;
+            string url = urlArray[songIndex];
+            webViewDisplay.CoreWebView2.Navigate(url);
+        }
+
+        private void clearButton_Click_1(object sender, EventArgs e)
+        {
+            titleText.Text = ""; //one to clear textbox
+            artistText.Text = String.Empty;
+            genreText.Clear();
+            yearText.Clear();
+            urlText.Clear();
+        }
+    }
+}
